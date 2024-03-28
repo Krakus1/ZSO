a14c2f733904c90e0bb1b8d527d0a9559464c99d
Piter <vbaapp@outlook.com>
2024-03-28 21:00:25 +0100

    add function createCars

--- creArr.c
+++ creArr.c
@@ -5,38 +5,38 @@ int main() {
     // 1 - fuel
     // 2 - lubricant
     // 3 - cooled_liquid
-
-int cars[10][3];
-    //paliwo        //smar          //plyn chlodniczy
-    cars[0][0]=8;	cars[0][1]=6;	cars[0][2]=5;
-    cars[1][0]=8;	cars[1][1]=10;	cars[1][2]=6;
-    cars[2][0]=5;	cars[2][1]=10;	cars[2][2]=7;
-    cars[3][0]=6;	cars[3][1]=5;	cars[3][2]=7;
-    cars[4][0]=7;	cars[4][1]=7;	cars[4][2]=5;
-    cars[5][0]=7;	cars[5][1]=8;	cars[5][2]=5;
-    cars[6][0]=8;	cars[6][1]=6;	cars[6][2]=8;
-    cars[7][0]=8;	cars[7][1]=6;	cars[7][2]=9;
-    cars[8][0]=8;	cars[8][1]=5;	cars[8][2]=9;
-    cars[9][0]=6;	cars[9][1]=6;	cars[9][2]=9;
-    cars[10][0]=10;	cars[10][1]=7;	cars[10][2]=8;
-    cars[11][0]=5;	cars[11][1]=9;	cars[11][2]=7;
-    cars[12][0]=6;	cars[12][1]=7;	cars[12][2]=5;
-    cars[13][0]=7;	cars[13][1]=5;	cars[13][2]=5;
-    cars[14][0]=5;	cars[14][1]=9;	cars[14][2]=5;
-    cars[15][0]=5;	cars[15][1]=10;	cars[15][2]=6;
-    cars[16][0]=9;	cars[16][1]=7;	cars[16][2]=7;
-    cars[17][0]=5;	cars[17][1]=8;	cars[17][2]=7;
-    cars[18][0]=10;	cars[18][1]=8;	cars[18][2]=5;
-    cars[19][0]=6;	cars[19][1]=8;	cars[19][2]=6;
-    cars[20][0]=5;	cars[20][1]=8;	cars[20][2]=9;
+    // 4 
+double cars[10][7];
+//nrPojazdu|paliwo|smar|plynChlodniczy|zuzyciePaliwa|zuzycieSmaru|zuzyciePlynuChlodniczego|
+    cars[0][0]=1;	cars[0][1]=100;	cars[0][2]=110; cars[0][3]=50; cars[0][4]=5; cars[0][5]=2.5; cars[0][6]=1.25;
+    // cars[1][0]=8;	cars[1][1]=10;	cars[1][2]=6;
+    // cars[2][0]=5;	cars[2][1]=10;	cars[2][2]=7;
+    // cars[3][0]=6;	cars[3][1]=5;	cars[3][2]=7;
+    // cars[4][0]=7;	cars[4][1]=7;	cars[4][2]=5;
+    // cars[5][0]=7;	cars[5][1]=8;	cars[5][2]=5;
+    // cars[6][0]=8;	cars[6][1]=6;	cars[6][2]=8;
+    // cars[7][0]=8;	cars[7][1]=6;	cars[7][2]=9;
+    // cars[8][0]=8;	cars[8][1]=5;	cars[8][2]=9;
+    // cars[9][0]=6;	cars[9][1]=6;	cars[9][2]=9;
+    // cars[10][0]=10;	cars[10][1]=7;	cars[10][2]=8;
+    // cars[11][0]=5;	cars[11][1]=9;	cars[11][2]=7;
+    // cars[12][0]=6;	cars[12][1]=7;	cars[12][2]=5;
+    // cars[13][0]=7;	cars[13][1]=5;	cars[13][2]=5;
+    // cars[14][0]=5;	cars[14][1]=9;	cars[14][2]=5;
+    // cars[15][0]=5;	cars[15][1]=10;	cars[15][2]=6;
+    // cars[16][0]=9;	cars[16][1]=7;	cars[16][2]=7;
+    // cars[17][0]=5;	cars[17][1]=8;	cars[17][2]=7;
+    // cars[18][0]=10;	cars[18][1]=8;	cars[18][2]=5;
+    // cars[19][0]=6;	cars[19][1]=8;	cars[19][2]=6;
+    // cars[20][0]=5;	cars[20][1]=8;	cars[20][2]=9;
 
     // Wypełnienie tablicy przykładowymi danymi
      int i, j;
 
     // Wyświetlenie zawartości tablicy
     printf("Value in array:\n");
-    for (i = 0; i < 10; i++) {
-        printf("Pozycja %d: %d, %d, %d\n", i+1, cars[i][0], cars[i][1], cars[i][2]);
+    for (i = 0; i < 1; i++) {
+        printf("Pozycja %f: %f, %f, %f, %f, %f, %f\n", cars[i][0], cars[i][1], cars[i][2], cars[i][3], cars[i][4], cars[i][5], cars[i][6]);
     }
     
     return 0;
--- projekt.c
+++ projekt.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h> 
+
+double** createCars() {
+    // Alokuje pamięć dla tablicy 10x7
+    double** cars = (double**)malloc(10 * sizeof(double*));
+    for (int i = 0; i < 10; i++) {
+        cars[i] = (double*)malloc(7 * sizeof(double));
+    }
+
+    // Wypełniam tablicę startowymi
+    cars[0][0]=1;	cars[0][1]=100;	cars[0][2]=110; cars[0][3]=50; cars[0][4]=5; cars[0][5]=2.5; cars[0][6]=1.25;
+
+    return cars;
+}
+
 
 int main() {
-    int liczba1, liczba2, suma;
-    
-    printf("Give me first number: ");
-    scanf("%d", &liczba1);
-    
-    printf("Give me second number: ");
-    scanf("%d", &liczba2);
-    
-    suma = liczba1 + liczba2;
-    
-    printf("Sum is: %d\n", suma);
-    
-    return 0;
+    double** cars = createCars();
+
+    int i;
+    printf("nrPojazdu|paliwo|smar|plynChlodniczy|zuzyciePaliwa|zuzycieSmaru|zuzyciePlynuChlodniczego|\n");
+    for (i = 0; i < 1; i++) {
+        printf("Pojazd numer %f: %f, %f, %f, %f, %f, %f\n", cars[i][0], cars[i][1], cars[i][2], cars[i][3], cars[i][4], cars[i][5], cars[i][6]);
+    }
 }
